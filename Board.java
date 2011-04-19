6823fdea4c6b959cd8bc4a20a668f7ccae2c78e8
Nathan Paulhus <nathanandrewpaulhus@gmail.com>
2011-04-19 23:15:12 +0200

    Updated descriptions

--- Board.java
+++ Board.java
@@ -1,3 +1,12 @@
+/* Board Class
+* (c) Nathan Paulhus, 2011
+* 
+* This class initalizes the GUI used in the program with two objects,
+* the textfield for the entered tags and the textarea, with a scrollpane,
+* for the "console" output. This is used simply so that the user can 
+* recieve updates on what the program is doing.
+*/
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -18,25 +27,29 @@ public class Board extends JPanel implements ActionListener{
 	
 	public Board(){
 		
+		//Set the default layout to the BorderLayour
 		setLayout(new BorderLayout());
 		
+		//Add the tags textfield and the console scroll pane to the main panel
 		add(tags,BorderLayout.NORTH);
 		add(scrollPane,BorderLayout.CENTER);
 		
-		
+		//Set the preferred size of the tags object to 400x25
 		tags.setPreferredSize(new Dimension(400,25));
-		
+		//Add the action listener to tags to listen for the enter key
 		tags.addActionListener(this);		
 	}
 	public void actionPerformed(ActionEvent e) {
+		//Announce the start message that the downloading is beginning
 		console.append("Initializing Downloads\n");
-		
+		//Get the entered tags from the tags text box
 		String input = tags.getText();
-		
+		//Create a new network object with the parameters equal to the tags
 		new Network(input);
 		
 	}
 	public void appendToConsole(String s){
+		//A public method allowing other methods to append to the console
 		console.append(s);
 	}
 	
--- Network.java
+++ Network.java
@@ -1,3 +1,13 @@
+/* Network Object
+ * (c) Nathan Paulhus, 2011
+ *
+ * This class is instantiated with a set of tags, which the class reads and turns into urls.
+ * After which, it attempts to download all the images on the page.
+ * When it runs out of images, it will attempt to go to the next page.
+ * If that contains no images, then it ends running thread and stops the object.
+ */
+
+
 import java.awt.image.RenderedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,7 +20,6 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
-
 public class Network implements Runnable{
 
 	private String t;
@@ -18,22 +27,26 @@ public class Network implements Runnable{
 	private boolean c = true;
 	
 	public Network(String ta){
+		//Create a new thread that runs inside this class
 		tr = new Thread(this);
 		tr.start();
+		//Set this classes tags value equal to the entered tags
 		this.t = ta;
 	}
 	@SuppressWarnings("deprecation")
 	public void run() {
-		
+			
 	       Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 		   
-	       while(true){
-	    	   	
+	       while(true){//Always
+	    	   	//Loop to 20k times, the max number of images Gelbooru will allow
 				for (int x = 0;x<20000;x+=25){
+					//If continue is no longer true, then stop the thread
 		    	   	if (!c){
 		    	   		tr.stop();
 		    	   	}else{
 						try {
+							//Start the main downloading process
 							initalizeProcess(t,x);
 						} catch (IOException e) {
 							Primary.getScreen().getPanel().appendToConsole("No more images remaining\n");
@@ -56,6 +69,7 @@ public class Network implements Runnable{
 		URL url = null;
 		//Assigns that url to a given url
 		try {
+			//Create the new url giving it both the tags from the users entry and the number from the loop above
 			url = new URL("http://gelbooru.com/index.php?page=post&s=list&tags="+tags+"&pid="+num);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -88,42 +102,45 @@ public class Network implements Runnable{
 		}
 	}
 	public static void downloadImage(String s,int i) throws IOException{
-		
+		//Get the name of the image as its index tag
 		String name = s.substring(s.indexOf("images/") + 11,s.lastIndexOf("."));
-		
+		//Get the image format (ie, jpg or png)
 		String format = s.substring(s.lastIndexOf(".",s.length()));
-		
+		//Initialize the URL object
 		URL url= null;
 		try {
+			//Set the url object equal to the provided url in the arguements
 			url = new URL(s);
 		} catch (MalformedURLException e) {
 			Primary.getScreen().getPanel().appendToConsole(e.toString()+"\n");
 		}
-		
+		//Create the GelbooruImages folder if it does not currently exist
 		File folder = new File("GelbooruImages");
 		if (!folder.exists()){
 			folder.mkdir();
 		}
-		
+		//Initalize the image to be rendered
 		RenderedImage bi = null;
 
 		try{
+			//Read the image into the RenderedImage object
 			bi = ImageIO.read(url);
-			
+			//Write the RenderedImage to a file
 			File f = new File(folder+"/"+name+format);
 			ImageIO.write(bi,format.replace(".",""),f);
-
+			//Announce on console that an image has been downloaded
 			Primary.getScreen().getPanel().appendToConsole("Image "+i+" Downloaded\n");
 		}catch(IllegalArgumentException e){
 			Primary.getScreen().getPanel().appendToConsole("Error Downloading Image\n");
 		}
 	}
 	public static ArrayList<String> getImageInfo(URL url) throws IOException{
-		
+		//Initalize the arraylist of strings and the buffered reader for reading the url
 		BufferedReader in = null;
 		ArrayList<String> info = new ArrayList<String>();
 		
 		try{
+			//Set the bufferedReader equal to the html stream of the url provided in the arguements
 			in = new BufferedReader(new InputStreamReader(url.openStream()));
 			
 			int x = 0;
@@ -132,7 +149,7 @@ public class Network implements Runnable{
 				if (inputLine.indexOf("class=\"thumb\"") > 0){
 					info.add(x,inputLine);
 					x++;
-
+					//Read all the lines of html, and add the lines that contain the image references to the arraylist of objects
 				}
 			}
 			in.close();
@@ -145,15 +162,16 @@ public class Network implements Runnable{
 		return info;
 	}
 	public static String getPID(String s){
-		
+		//The procedure to get the pictures id, simply referencing the location of the src tag start and end.
 		int i = s.indexOf("<img src=\"");	
 		int j = s.indexOf("\" alt=\"");
 		String st = (s.substring(i+10,j));
-	
+
 		return st;
 	}
 	public static String getImageUrl(String s){
-		
+		//Method to create the url
+		//Get the location of the necessary parts of the tags
 		int chars_start = s.indexOf("umbs/") + 5;
 		int chars_end = s.indexOf("/thumbnail");
 		String chars = s.substring(chars_start,chars_end);
@@ -161,7 +179,7 @@ public class Network implements Runnable{
 		int id_start = s.indexOf("nail_") + 5;
 		int id_end = s.indexOf("?");
 		String id = s.substring(id_start,id_end);
-		
+		//Concatenate these parts into the url.
 		String url = ("http://img4.gelbooru.com//images/"+chars+"/"+id);		
 		return url;
 		
